31ade8e9b61a4ffd817511ebd7218ab6c3b8987b
Ramon Cisneros <cisneroslopezramon1@gmail.com>
2024-02-12 02:35:06 -0600

    Add increment and decrement buttons

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,50 @@ function App() {
     setCantidad(+e.target.value);
     
   }
+
+  function handleClickDecremento() {
+    const valor = cantidad - step;
+
+    if (valor < min) {
+      alert('Cantidad no valida');
+      return;
+    }
+
+
+    setCantidad(valor);
+  }
+
+  function handleClickIncremento() {
+    const valor = cantidad + step;
+
+    if (valor > max) {
+      alert('Cantidad no valida');
+      return;
+    }
+
+
+    setCantidad(valor);
+  }
   
 
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
+
+      <div className='flex justify-between my-6'>
+        <button 
+          type='button'
+          className='h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-lime-500 hover:ring-offset-2'
+          onClick={handleClickDecremento}
+        >-</button>
+
+        <button 
+          type='button'
+          className='h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-lime-500 hover:ring-offset-2'
+          onClick={handleClickIncremento}
+        >+</button>
+      
+      </div>
 
       <input 
         type="range"
