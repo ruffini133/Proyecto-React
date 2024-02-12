4699c1f8be404525e2bf157f2562f94d8307d413
Ramon Cisneros <cisneroslopezramon1@gmail.com>
2024-02-12 01:15:04 -0600

    Se agregó un input range y variables: min, max y step

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ function App() {
   const [cantidad, setCantidad] = useState(10000);
   console.log(cantidad);
 
+  const min = 0;
+  const max = 20000;
+  const step = 100;
+
   function handleChange(e) {
     setCantidad(+e.target.value);
     
@@ -19,9 +23,13 @@ function App() {
         type="range"
         className='w-full h-6 bg-gray-200 accent-lime-500 hover-accent-lime-'
         onChange={handleChange}
+        min={min}
+        max={max}
+        step={step}
+        value={cantidad}
       />
 
-      {cantidad}
+      <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>{cantidad}</p>
     </div>
   )
 }
