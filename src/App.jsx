e464f179c31c7c1fb6c94505146db73822019191
Ramon Cisneros <cisneroslopezramon1@gmail.com>
2024-02-12 03:11:10 -0600

    Add select element for choosing loan term

--- src/App.jsx
+++ src/App.jsx
@@ -70,6 +70,18 @@ function App() {
       <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>
         {formatearDinero(cantidad)}
       </p>
+
+      <h2 className='text-2xl font-extrabold text-gray-500 text-center'>
+        Elige un <span className='text-indigo-600'>plazo</span> a pagar
+      </h2>
+
+      <select
+        className='mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500'
+      >
+        <option value="6">6 meses</option>
+        <option value="12">12 meses</option>
+        <option value="24">24 meses</option>
+      </select>
     </div>
   )
 }
